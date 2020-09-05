17f5471177d73fdf5af84a29321808f6bf965efe
chris west <c@xvxx.io>
2020-09-05 16:47:47 -0700

    stringz

--- src/expr.rs
+++ src/expr.rs
@@ -10,7 +10,8 @@ pub enum Expr {
 impl Expr {
     pub fn to_string(&self) -> String {
         match self {
-            Expr::String(s) | Expr::Word(s) => s.clone(),
+            Expr::String(s) => format!(r#""{}""#, s),
+            Expr::Word(s) => s.clone(),
             Expr::Bool(b) => format!("{}", b),
             Expr::Number(n) => format!("{}", n),
             Expr::Call(name, args) => format!(

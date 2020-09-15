1b81352090b5437b33b552abbeff263632c0ce38
chris west <c@xvxx.io>
2020-09-15 12:27:01 -0700

    hook up example tests

--- tests/examples_test.rs
+++ tests/examples_test.rs
@@ -1,10 +1,22 @@
 use {
     hatter,
-    std::{fs, io, path},
+    std::{
+        fs,
+        io::{self, Write},
+        path,
+    },
 };
 
 #[test]
 fn test_examples() -> io::Result<()> {
+    if shell("which", &["hpp"])? == "" {
+        let banner = 50;
+        println!("\n{}", "-".repeat(banner));
+        println!("Please install hpp to run tests:\n");
+        println!("https://github.com/xvxx/hpp-cli");
+        println!("{}\n", "-".repeat(banner));
+        return Err(io::Error::new(io::ErrorKind::Other, "hpp not found"));
+    }
     test_dir("./examples/")
 }
 
@@ -21,19 +33,38 @@ fn test_dir<P: AsRef<path::Path>>(dir: P) -> io::Result<()> {
                 .replace("./examples/", "./tests/examples/")
                 .replace(".hat", ".html");
 
-            println!("{}", test_path);
-            assert_eq!(
-                fs::read_to_string(&test_path).map_err(|e| {
-                    io::Error::new(io::ErrorKind::NotFound, format!("{}: {}", e, test_path))
-                })?,
-                hatter::to_html(&source)
-                    .map_err(|err| {
-                        hatter::print_error(path, source, err);
-                        assert!(false);
-                    })
-                    .unwrap()
-            );
+            let tmp_path = "/tmp/hatter.test";
+            let mut file = fs::File::create(tmp_path)?;
+            write!(file, "{}", hatter::to_html(&source)?)?;
+            let (expected, actual) = (pretty(&test_path)?, pretty(tmp_path)?);
+            if expected != actual {
+                println!("=== EXPECTED ==========\n{}", expected);
+                println!("=== ACTUAL ==========\n{}", actual);
+                assert!(false);
+            }
         }
     }
     Ok(())
+}
+
+/// Pretty print the HTML file at `path`.
+fn pretty(path: &str) -> io::Result<String> {
+    shell("hpp", &[path])
+}
+
+/// Run a script and return its output.
+fn shell(path: &str, args: &[&str]) -> io::Result<String> {
+    let output = std::process::Command::new(path).args(args).output()?;
+    let out = if output.status.success() {
+        output.stdout
+    } else {
+        output.stderr
+    };
+    match std::str::from_utf8(&out) {
+        Ok(s) => Ok(s.trim().to_string()),
+        Err(e) => Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            e.to_string(),
+        )),
+    }
 }

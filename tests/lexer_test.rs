942c9b339ba89ac65dd50f5daba5d51d1710bc6e
chris west <c@xvxx.io>
2020-09-18 10:07:53 -0700

    lexer tests

--- tests/lexer_test.rs
+++ tests/lexer_test.rs
@@ -1,4 +1,4 @@
-use hatter::{scan, Syntax};
+use hatter::{scan, Syntax::*};
 
 macro_rules! scan {
     ($code:expr) => {
@@ -7,49 +7,108 @@ macro_rules! scan {
 }
 
 macro_rules! scan_test {
-    ($name:ident, $code:expr, $($kind:ident),+) => {
+    ($name:ident, $code:expr, $($kind:expr,)+) => {
         #[test]
         fn $name() {
-            let nodes = scan!("1 + 1");
-            $(assert_eq!(Syntax::$kind, nodes.remove(0).kind);)+
+            let mut nodes = scan!($code);
+            let debug = nodes.clone();
+            $(
+                let node = nodes.remove(0);
+                if node.kind != $kind {
+                    println!("{:#?}", debug);
+                }
+                assert_eq!($kind, node.kind);
+            )+
         }
     };
+    ($name:ident, $code:expr, $($kind:expr),+) => {
+        scan_test!($name, $code, $($kind,)+);
+    }
 }
 
-scan_test!(one_plus_one, "1 + 1", Number, Op, Number);
+////
+// Numbers
 
-#[test]
-fn test_comment() {
-    let nodes = scan!("1 + 1");
-    println!("{:?}", scan!("1 + 1"));
-    assert!(false);
-}
+scan_test!(positive_number, "100", Number);
+scan_test!(negative_number, "-400", Number);
+scan_test!(underscore_number, "1_422_200", Number);
+scan_test!(negative_underscore_number, "-1_422_200", Number);
+scan_test!(binary_number, "0b101", Number);
+scan_test!(octal_number, "0o755", Number);
+scan_test!(hex_number, "0xdeadbeef", Number);
+scan_test!(float_number, "3.14", Number);
+scan_test!(negative_float_number, "-1230.3552", Number);
 
-/*
-code: number, string, word, or operator
-comments:
-    - # to end of line
-number:
-    - 200
-    - -10_150_203
-    - 0b101
-    - 0o123
-    - 0xdeadbeef
-    - 3.14
-    - -102.123
-string:
-    "hey pal"
-    'hey pal'
-    `hey pal`
-    """hey pal"""
-    '''hey pal'''
-    ```hey pal```
-word:
-    sup
-    Yes
-    some_thing
-    can-include-dashes-{and brackets}
-operator:
-    no letters, no numbers
-    ex: + - * / . = :=
-    */
+////
+// Comments
+
+scan_test!(basic_comment, "123 # Something something.", Number);
+scan_test!(
+    another_basic_comment,
+    "456\n# Something something.\n789",
+    Number,
+    Special(';'),
+    Special(';'),
+    Number,
+    Special(';'),
+);
+
+////
+// Strings
+
+scan_test!(basic_string, r#""just a regular string""#, String(true));
+scan_test!(single_quote_string, r#"'single quote'"#, String(false));
+scan_test!(
+    escaped_single_quote_string,
+    r#"'what\'s up'"#,
+    String(false)
+);
+scan_test!(grave_string, r#"`one two three`"#, String(false));
+scan_test!(triple_string, r#" """one two three""" "#, String(false));
+scan_test!(
+    triple_single_string,
+    r#"'''one two three'''"#,
+    String(false)
+);
+scan_test!(triple_grave_string, r#"```one two three```"#, String(false));
+scan_test!(
+    triple_string_with_breaks,
+    r#""""
+one
+two
+three
+""""#,
+    String(false)
+);
+
+////
+// Words
+
+scan_test!(simple_word, "sup", Word);
+scan_test!(title_word, "Yep", Word);
+scan_test!(underscore_word, "some_thing", Word);
+scan_test!(underscore_word2, "_some_other_thing_", Word);
+scan_test!(dash_word, "some-thing", Word);
+scan_test!(dash_word2, "some-thing-", Word);
+scan_test!(numbered_word, "one123", Word);
+scan_test!(word_with_brackets, word-{with brackets}-in-it, Word)
+scan_test!(word_ending_with_brackets, word-ending-{with brackets}, Word)
+scan_test!(word_ending_with_question, word-ending-in?, Word)
+scan_test!(and_is_a_word, "and", Word);
+
+////
+// Ops
+
+scan_test!(simple_op, "+", Op);
+scan_test!(lonley_dash_is_an_op, "-", Op);
+scan_test!(two_char_op, ":=", Op);
+scan_test!(gt_op, ">", Op);
+scan_test!(random_op, "*@$%^$*", Op);
+scan_test!(dot_op, ".", Op);
+scan_test!(double_dot_op, "..", Op);
+scan_test!(question_op, "?", Op);
+scan_test!(bang_op, "!", Op);
+
+scan_test!(bracket_is_op, "<", Op);
+scan_test!(bracket_followed_by_word, "<div>", Op, Word, Op);
+

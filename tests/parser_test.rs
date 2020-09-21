5374ab7ded86bcc506510b94c1bf5d2cd8c1e783
chris west <c@xvxx.io>
2020-09-21 14:32:31 -0700

    fix attr expectations

--- tests/parser_test.rs
+++ tests/parser_test.rs
@@ -615,7 +615,7 @@ parse_test!(close_shortcut, "<b> Hey <i> there </> fren ", {
 parse_test!(basic_id_shortcut, "<b#shout> Hey yo", {
     let mut b = tag!("b");
     b.set_id(word!("shout"));
-    b.set_body(vec![word!("Hey"), word!("yo")]);
+    b.set_body(vec![string!("Hey yo")]);
     Stmt::Tag(b)
 });
 
@@ -636,21 +636,21 @@ parse_test!(basic_classes, "<div.main.markdown> Yada yada... ", {
 
 parse_test!(basic_name_shortcut, "<input@text/>", {
     let mut tag = tag!("input");
-    tag.add_attr(word!("name"), string!("text"));
+    tag.add_attr(string!("name"), string!("text"));
     tag.close();
     Stmt::Tag(tag)
 });
 
 parse_test!(basic_type_shortcut, "<input:submit/> ", {
     let mut tag = tag!("input");
-    tag.add_attr(word!("type"), string!("submit"));
+    tag.add_attr(string!("type"), string!("submit"));
     tag.close();
     Stmt::Tag(tag)
 });
 
 parse_test!(mixed_shortcuts, "<div#main.markdown/> ", {
     let mut tag = tag!("div");
-    tag.set_id(word!("main"));
+    tag.set_id(string!("main"));
     tag.add_class(string!("markdown"));
     tag.close();
     Stmt::Tag(tag)
@@ -658,16 +658,16 @@ parse_test!(mixed_shortcuts, "<div#main.markdown/> ", {
 
 parse_test!(mixed_input_shortcuts, "<input#focused:text@search  /> ", {
     let mut tag = tag!("input");
-    tag.set_id(word!("focused"));
-    tag.add_attr(word!("type"), string!("text"));
-    tag.add_attr(word!("name"), string!("search"));
+    tag.set_id(string!("focused"));
+    tag.add_attr(string!("type"), string!("text"));
+    tag.add_attr(string!("name"), string!("search"));
     tag.close();
     Stmt::Tag(tag)
 });
 
 parse_test!(basic_attribute, r#"<div data-id="45">"#, {
     let mut tag = tag!("div");
-    tag.add_attr(word!("data-id"), string!("45"));
+    tag.add_attr(string!("data-id"), string!("45"));
     Stmt::Tag(tag)
 });
 
@@ -676,8 +676,8 @@ parse_test!(
     r#"<input name="thing" placeholder="Other..."/>"#,
     {
         let mut tag = tag!("input");
-        tag.add_attr(word!("name"), string!("thing"));
-        tag.add_attr(word!("placeholder"), string!("Other..."));
+        tag.add_attr(string!("name"), string!("thing"));
+        tag.add_attr(string!("placeholder"), string!("Other..."));
         tag.close();
         Stmt::Tag(tag)
     }
@@ -685,14 +685,14 @@ parse_test!(
 
 parse_test!(js_attributes, "<div onclick=(alert('lol'))>Click me", {
     let mut tag = tag!("div");
-    tag.add_attr(word!("onclick"), string!("javascript..."));
+    tag.add_attr(string!("onclick"), string!("javascript..."));
     tag.set_body(vec![string!("Click me")]);
     Stmt::Tag(tag)
 });
 
 parse_test!(simple_code_attributes, "<div data-id=page.id>", {
     let mut tag = tag!("div");
-    tag.add_attr(word!("data-id"), call!(".", word!("page"), word!("id")));
+    tag.add_attr(string!("data-id"), call!(".", word!("page"), word!("id")));
     Stmt::Tag(tag)
 });
 
@@ -704,7 +704,7 @@ parse_test!(shorthand_conditionals, "<div#id=has-id>", {
 
 parse_test!(code_expr_attributes, "<div data-value={2 + 3}>", {
     let mut tag = tag!("div");
-    tag.add_attr(word!("data-value"), string!("{2 + 3}"));
+    tag.add_attr(string!("data-value"), string!("{2 + 3}"));
     Stmt::Tag(tag)
 });
 
@@ -713,14 +713,14 @@ parse_test!(
     "<div#id.class1.class-2=is-it? :why-not @sure onclick=(alert(`it's ${2 + 2}`)) data-id=123 data-{value}=compute(value) />",
     {
         let mut tag = tag!("div");
-        tag.set_id(word!("id"));
+        tag.set_id(string!("id"));
         tag.add_class(string!("class1"));
         tag.add_class(call!("when", word!("is-it?"), string!("class-2")));
-        tag.add_attr(word!("type"), string!("why-not"));
-        tag.add_attr(word!("name"), string!("sure"));
-        tag.add_attr(word!("onclick"), string!("JS..."));
-        tag.add_attr(word!("data-id"), num!(123));
-        tag.add_attr(word!("data-{value}"), call!("compute", word!("value")));
+        tag.add_attr(string!("type"), string!("why-not"));
+        tag.add_attr(string!("name"), string!("sure"));
+        tag.add_attr(string!("onclick"), string!("JS..."));
+        tag.add_attr(string!("data-id"), num!(123));
+        tag.add_attr(string!("data-{value}"), call!("compute", word!("value")));
         tag.close();
         Stmt::Tag(tag)
     }
@@ -731,7 +731,7 @@ parse_test!(
     r#"<#my-id> Just some <.bold>cool</> "content.""#,
     {
         let mut tag = tag!("div");
-        tag.set_id(word!("my-id"));
+        tag.set_id(string!("my-id"));
         let mut bold = tag!("div");
         bold.add_class(string!("bold"));
         bold.set_body(vec![word!("cool")]);
@@ -765,14 +765,14 @@ parse_test!(
 "#,
     {
         let mut form = tag!("form");
-        form.add_attr(word!("method"), string!("GET"));
-        form.add_attr(word!("action"), string!("/search"));
+        form.add_attr(string!("method"), string!("GET"));
+        form.add_attr(string!("action"), string!("/search"));
         let mut text = tag!("input");
-        text.add_attr(word!("type"), string!("text"));
-        text.add_attr(word!("name"), string!("query"));
+        text.add_attr(string!("type"), string!("text"));
+        text.add_attr(string!("name"), string!("query"));
         text.close();
         let mut btn = tag!("input");
-        btn.add_attr(word!("type"), string!("submit"));
+        btn.add_attr(string!("type"), string!("submit"));
         btn.close();
         form.set_body(vec![text.into(), btn.into()]);
         Stmt::Tag(form)
@@ -792,25 +792,25 @@ parse_test!(
 "#,
     {
         let mut form = tag!("form");
-        form.add_attr(word!("method"), string!("POST"));
-        form.add_attr(word!("action"), string!("/info"));
+        form.add_attr(string!("method"), string!("POST"));
+        form.add_attr(string!("action"), string!("/info"));
 
         let mut h3 = tag!("h3");
         h3.set_body(vec![string!("Your Information")]);
 
         let mut lbl1 = tag!("label");
         let mut name = tag!("input");
-        name.add_attr(word!("name"), string!("name"));
-        name.add_attr(word!("type"), string!("text"));
-        name.add_attr(word!("placeholder"), string!("Name..."));
+        name.add_attr(string!("name"), string!("name"));
+        name.add_attr(string!("type"), string!("text"));
+        name.add_attr(string!("placeholder"), string!("Name..."));
         name.close();
         lbl1.set_body(vec![name.into()]);
 
         let mut lbl2 = tag!("label");
         let mut age = tag!("input");
-        age.add_attr(word!("name"), string!("age"));
-        age.add_attr(word!("type"), string!("text"));
-        age.add_attr(word!("placeholder"), string!("Age..."));
+        age.add_attr(string!("name"), string!("age"));
+        age.add_attr(string!("type"), string!("text"));
+        age.add_attr(string!("placeholder"), string!("Age..."));
         age.close();
         lbl2.set_body(vec![age.into()]);
 
@@ -818,10 +818,10 @@ parse_test!(
         br.close();
 
         let mut submit = tag!("input");
-        submit.add_attr(word!("type"), word!("submit"));
+        submit.add_attr(string!("type"), word!("submit"));
 
         let mut reset = tag!("input");
-        reset.add_attr(word!("type"), word!("reset"));
+        reset.add_attr(string!("type"), word!("reset"));
 
         form.set_body(
             vec![h3, lbl1, lbl2, br, submit, reset]
@@ -844,21 +844,21 @@ parse_test!(
 "#,
     {
         let mut ul = tag!("ul");
-        ul.set_id(word!("menu"));
+        ul.set_id(string!("menu"));
 
         let mut burger = tag!("li");
-        burger.add_class(word!("item"));
-        burger.set_id(word!("burger"));
+        burger.add_class(string!("item"));
+        burger.set_id(string!("burger"));
         burger.set_body(vec![word!("Burger")]);
 
         let mut fries = tag!("li");
-        fries.add_class(word!("item"));
-        fries.set_id(word!("fries"));
+        fries.add_class(string!("item"));
+        fries.set_id(string!("fries"));
         fries.set_body(vec![word!("Fries")]);
 
         let mut shake = tag!("li");
-        shake.add_class(word!("item"));
-        shake.set_id(word!("shake"));
+        shake.add_class(string!("item"));
+        shake.set_id(string!("shake"));
         shake.set_body(vec![word!("Milkshake")]);
 
         ul.set_body(vec![burger.into(), fries.into(), shake.into()]);

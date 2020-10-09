7f8fdc228a6689f1e44205375fb63c6b6f79269f
chris west <c@xvxx.io>
2020-10-09 01:01:46 -0700

    generic

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+//! Hatter is a small, whitespace sensitive templating language with
+//! HTML support built right in. Its HTML features and syntax are a
+//! cheap knock off of Imba, except Hatter produces raw, static HTML -
+//! no JavaScript in sight.
+
+//! Hatter can be used to generate static web sites or to render
+//! server side content in a good ol' fashioned web application -
+//! either with Vial or your Rust web framework of choice.
+
+ //! If you're feeling adventerous, or mad as a hatter, you can use
+ //! the standalone binary to turn templates into HTML files or
+ //! include the zero-dependency Rust library in your (web/cli/?)
+ //! application.
+
 #![deny(unused_must_use)]
 #![deny(unreachable_patterns)]
 
@@ -37,4 +51,5 @@ pub use {
     template::Template,
 };
 
+/// Alias for a generic Result type.
 pub type Result<T> = std::result::Result<T, Error>;
